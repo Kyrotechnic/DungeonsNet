2a52cffd5f5cfca488dcf7cf5ae23bfe130c42ad
Ethan <122495136+nolifejavadeveloper@users.noreply.github.com>
2024-06-08 12:09:02 -0400

    i forgot to save

--- Dungeons.Server/src/Protocol/Clientbound/Play/ClientEntityProperties.cs
+++ Dungeons.Server/src/Protocol/Clientbound/Play/ClientEntityProperties.cs
@@ -16,7 +16,22 @@ public class ClientEntityProperties : IClientPacket
     {
         IBuffer buffer = IBuffer.Create();
 
+        buffer.WriteVarInt(EntityId);
+        buffer.WriteInt(Property.Length);
         
+        for (int i = 0; i < Property.Length; i++) {
+            EntityProperty current = Property[i];
+
+            buffer.WriteString(current.Key);
+            buffer.WriteDouble(current.Value);
+            buffer.WriteVarInt(current.Modifiers.Length);
+
+            for (int j = 0; j < current.Modifiers.Length; j++) {
+                AttributeModifier currentModifier = current.Modifiers[i];
+                buffer.WriteString(currentModifier.Key);
+                buffer.WriteString(currentModifier.Value);
+            }
+        }
 
         return buffer;
     }
